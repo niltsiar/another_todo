a814c6e0d8ee49bbc933bdae380b55a6519785f2
Borja Quevedo <borjota@gmail.com>
2025-06-15 09:22:57 +0200

    🔧 build: use direct plugin IDs for KSP, KAPT, and Hilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@
 buildscript {
     dependencies {
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.51")
-        classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:2.0.21-1.0.19")
     }
 }
 
@@ -10,4 +9,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.19" apply false
+    id("org.jetbrains.kotlin.kapt") version "2.0.21" apply false
+    id("com.google.dagger.hilt.android") version "2.51" apply false
 }
