df6713c45468f9184a9a3c792a0e241e40a362a9
Borja Quevedo <borjota@gmail.com>
2025-06-15 09:11:29 +0200

    🔧 chore: update theme usage in MainActivity.kt

--- app/src/main/java/dev/niltsiar/anothertodo/MainActivity.kt
+++ app/src/main/java/dev/niltsiar/anothertodo/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dev.niltsiar.anothertodo.ui.theme.AnotherTODOTheme
+import dev.niltsiar.anothertodo.ui.theme.AnotherTodoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AnotherTODOTheme {
+            AnotherTodoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    AnotherTODOTheme {
+    AnotherTodoTheme {
         Greeting("Android")
     }
 }
