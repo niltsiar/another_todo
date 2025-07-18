5f23d9edd0c4ef7297b1c142856545fe0cdc82ff
Borja Quevedo <borjota@gmail.com>
2025-07-18 13:01:57 +0200

    📦 NEW: Add kotlinx-collections-immutable dependency

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.kotlinx.datetime)
+    implementation(libs.kotlinx.collections.immutable)
 
     // Arrow
     implementation(libs.arrow.core)
