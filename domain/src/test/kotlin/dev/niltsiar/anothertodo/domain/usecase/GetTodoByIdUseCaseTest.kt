c094442ed4d24bfb8ffdb5da7cdcbac433dd7f5b
Borja Quevedo <borjota@gmail.com>
2025-07-18 13:11:15 +0200

    ✅ TEST: Implement property-based testing for GetTodoByIdUseCase

--- domain/src/test/kotlin/dev/niltsiar/anothertodo/domain/usecase/GetTodoByIdUseCaseTest.kt
+++ domain/src/test/kotlin/dev/niltsiar/anothertodo/domain/usecase/GetTodoByIdUseCaseTest.kt
@@ -6,54 +6,104 @@ import dev.niltsiar.anothertodo.domain.model.Priority
 import dev.niltsiar.anothertodo.domain.model.TodoItem
 import dev.niltsiar.anothertodo.domain.repository.TodoError
 import dev.niltsiar.anothertodo.domain.repository.TodoRepository
+import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.long
+import io.kotest.property.arbitrary.positiveInts
+import io.kotest.property.arbitrary.string
+import io.kotest.property.checkAll
+import io.kotest.property.forAll
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
-import org.junit.Before
-import org.junit.Test
+import kotlinx.datetime.LocalDateTime
 
-class GetTodoByIdUseCaseTest {
+class GetTodoByIdUseCaseTest : StringSpec({
+    val repository = mockk<TodoRepository>()
+    val useCase = GetTodoByIdUseCase(repository)
 
-    private lateinit var repository: TodoRepository
-    private lateinit var useCase: GetTodoByIdUseCase
+    "should return Right with todo when repository returns success" {
+        checkAll(Arb.long(min = 1)) { todoId ->
+            runTest {
+                // Given
+                val expectedTodo = TodoItem(
+                    id = todoId,
+                    title = "Test Todo $todoId",
+                    description = "Test Description for todo $todoId",
+                    isCompleted = false,
+                    priority = Priority.MEDIUM
+                )
+                coEvery { repository.getTodoById(todoId) } returns expectedTodo.right()
 
-    @Before
-    fun setup() {
-        repository = mockk()
-        useCase = GetTodoByIdUseCase(repository)
+                // When
+                val result = useCase(todoId)
+
+                // Then
+                result shouldBe expectedTodo.right()
+            }
+        }
     }
 
-    @Test
-    fun `should return Right with todo when repository returns success`() = runTest {
-        // Given
-        val todoId = 1L
-        val expectedTodo = TodoItem(
-            id = todoId,
-            title = "Test Todo",
-            description = "Test Description",
-            isCompleted = false,
-            priority = Priority.MEDIUM
-        )
-        coEvery { repository.getTodoById(todoId) } returns expectedTodo.right()
+    "should return Left with TodoNotFound when repository returns error for non-existent todo" {
+        forAll(Arb.long(min = 1)) { todoId ->
+            runTest {
+                // Given
+                coEvery { repository.getTodoById(todoId) } returns TodoError.TodoNotFound.left()
 
-        // When
-        val result = useCase(todoId)
+                // When
+                val result = useCase(todoId)
 
-        // Then
-        result shouldBe expectedTodo.right()
+                // Then
+                result shouldBe TodoError.TodoNotFound.left()
+            }
+            true
+        }
     }
 
-    @Test
-    fun `should return Left with error when repository returns error`() = runTest {
-        // Given
-        val todoId = 1L
-        coEvery { repository.getTodoById(todoId) } returns TodoError.TodoNotFound.left()
+    "should return Left with DatabaseError when repository encounters database issues" {
+        forAll(Arb.long(min = 1)) { todoId ->
+            runTest {
+                // Given
+                coEvery { repository.getTodoById(todoId) } returns TodoError.DatabaseError.left()
 
-        // When
-        val result = useCase(todoId)
+                // When
+                val result = useCase(todoId)
 
-        // Then
-        result shouldBe TodoError.TodoNotFound.left()
+                // Then
+                result shouldBe TodoError.DatabaseError.left()
+            }
+            true
+        }
     }
-}
+
+    "should handle todos with various properties correctly" {
+        checkAll(
+            Arb.long(min = 1),
+            Arb.string(minSize = 1, maxSize = 100),
+            Arb.string(minSize = 0, maxSize = 500)
+        ) { todoId, title, description ->
+            runTest {
+                // Given
+                val expectedTodo = TodoItem(
+                    id = todoId,
+                    title = title,
+                    description = description,
+                    isCompleted = todoId % 2 == 0L, // Alternate between true and false
+                    priority = when (todoId % 3) {
+                        0L -> Priority.LOW
+                        1L -> Priority.MEDIUM
+                        else -> Priority.HIGH
+                    }
+                )
+                coEvery { repository.getTodoById(todoId) } returns expectedTodo.right()
+
+                // When
+                val result = useCase(todoId)
+
+                // Then
+                result shouldBe expectedTodo.right()
+            }
+        }
+    }
+})
