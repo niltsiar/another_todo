75b6cdf3126bf98a9dd19019bcd54bf75ef8a367
Borja Quevedo <borjota@gmail.com>
2025-07-18 13:02:32 +0200

    ✨ FEATURE: Use serializable data classes in TodoDestinations

--- presentation/src/main/java/dev/niltsiar/anothertodo/presentation/ui/navigation/TodoDestinations.kt
+++ presentation/src/main/java/dev/niltsiar/anothertodo/presentation/ui/navigation/TodoDestinations.kt
@@ -2,16 +2,32 @@ package dev.niltsiar.anothertodo.presentation.ui.navigation
 
 import androidx.navigation.NavType
 import androidx.navigation.navArgument
+import kotlinx.serialization.Serializable
+
+sealed interface TodoDestination {
+    val route: String
+}
 
 object TodoDestinations {
-    const val TODO_LIST = "todo_list"
-    const val ADD_EDIT_TODO = "add_edit_todo"
-    
-    val arguments = listOf(
-        navArgument("todoId") {
-            type = NavType.StringType
-            nullable = true
-            defaultValue = null
+    const val TODO_LIST_ROUTE = "todo_list"
+    const val ADD_EDIT_TODO_ROUTE = "add_edit_todo"
+
+    object TodoList : TodoDestination {
+        override val route = TODO_LIST_ROUTE
+    }
+
+    @Serializable
+    data class AddEditTodo(val todoId: Long? = null) : TodoDestination {
+        override val route = "$ADD_EDIT_TODO_ROUTE/${todoId ?: "new"}"
+
+        companion object {
+            const val route = "$ADD_EDIT_TODO_ROUTE/{todoId}"
+            val arguments = listOf(
+                navArgument("todoId") {
+                    type = NavType.StringType
+                    nullable = false
+                }
+            )
         }
-    )
+    }
 }
